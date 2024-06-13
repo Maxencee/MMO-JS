b1486825ff35f75336b2e7ea4729ca9174e1ef86
Maxence <maxence.gvr@gmail.com>
2024-06-13 09:52:23 +0200

    Added scene loader

---
+++ src/scenes/SandboxScene.js
@@ -0,0 +1,72 @@
+import Scene from "../classes/Scene";
+import PlayerController from "../PlayerController";
+import LightEnvironment from "../classes/LightEnvironment";
+import BoundingBox from "../entities/BoundingBox";
+import Floor from "../entities/Floor";
+
+import PropDynamic from "../entities/PropDynamic";
+import { LoopOnce, Vector3 } from "three";
+import BoundingBoxInteractable from "../entities/BoundingBoxInteractable";
+import PropStatic from "../entities/PropStatic";
+import PropInteractable from "../entities/PropInteractable";
+import Process from "../classes/Process";
+
+export default class SandboxScene extends Scene {
+    constructor (backgroundColor) {
+        super(backgroundColor);
+
+        this.add(new LightEnvironment(0xffffff, 2));
+
+        const floor = new Floor();
+        this.add(floor);
+      
+        const wall = new PropStatic("assets/models/wall/scene.gltf", {
+          bounding: null,
+          scale: new Vector3(0.0045, 0.0045, 0.0045),
+          position: new Vector3(0, -1.8, 0.4),
+          shadow: PropStatic.RECEIVE_SHADOW,
+        });
+      
+        wall.position.x = 4.5;
+        wall.position.z = 3;
+        this.add(wall);
+      
+        const chest = new PropInteractable("assets/models/chest/source/Chest.fbx", {
+          texture: 'assets/models/chest/textures/Chest_D.png',
+          bounding: 0xffffff,
+          scale: new Vector3(0.008, 0.008, 0.008),
+          position: new Vector3(0, 0, 0),
+          shadow: PropStatic.RECEIVE_SHADOW,
+        });
+
+        chest.interact = function () {
+          let animation = this.mixer.clipAction(this.children[0].animations[0]);
+          animation.loop = LoopOnce;
+          animation.clampWhenFinished = true;
+          animation.play();
+          console.log(animation);
+        }
+
+        chest.onModelLoaded = function () {
+          Process.addToQueue(() => {
+            chest.mixer.update(chest.clock.getDelta());
+          });
+        }
+
+        console.log(chest);
+      
+        chest.position.x = 4;
+        chest.position.z = -4;
+        this.add(chest);
+      
+        const jumpWall = new BoundingBoxInteractable(3, 0.5, 1, 0xff9d4d);
+      
+        jumpWall.position.x = -4;
+        jumpWall.position.z = -4;
+        this.add(jumpWall);
+      
+        const player = new PlayerController();
+      
+        this.add(player);
+    }
+}
