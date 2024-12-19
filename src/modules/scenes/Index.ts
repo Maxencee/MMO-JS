5b2e37fe78bd84ffbe81a0771b8ff8c636b03cf2
Maxence <maxence.gvr@gmail.com>
2024-12-19 13:25:59 +0100

    feature: holographic in opens

--- src/modules/scenes/Index.ts
+++ src/modules/scenes/Index.ts
@@ -15,22 +15,12 @@ export default class Index extends THREE.Scene {
     BoundingBox.setMode('wireframe');
     Process.setCamera(new PlayerCamera);
 
-    const floor = new BoundingBox(20, 0.1, 20) as BoundingBox & { isFloor: boolean };
-    floor.add(new THREE.Mesh(new THREE.BoxGeometry(20, 0.1, 20), new THREE.MeshBasicMaterial({ color: 0x504763 })));
+    const floor = new BoundingBox(20, 0.1, 20);
+    floor.add(new THREE.Mesh(new THREE.BoxGeometry(20, 0.1, 20), new THREE.MeshBasicMaterial({ color: 0x4f4f4f })));
     floor.isFloor = true;
-
-    const vending = new PropInteractable('assets/models/vending-machine/model.fbx', {
-      scale: 1.2,
-      rotation: new THREE.Euler(0, -Math.PI/2, 0),
-      position: new THREE.Vector3(3, 0, -4)
-    });
-
-    vending.position.y += 0.4;
-
-    this.add(vending);
 
     this.add(floor);
     this.add(new PlayerController);
-    this.add(new LightEnvironment(0xfafffb, 1));
+    this.add(new LightEnvironment(0xfafffb, 1.5));
   }
 }
