5986d00d94f34aab8984630dea93dbd2eb03e2fd
Maxence <maxence.gvr@gmail.com>
2024-12-22 14:40:45 +0100

    chore: clean up

--- src/modules/scenes/Index.ts
+++ src/modules/scenes/Index.ts
@@ -1,26 +1,9 @@
 import * as THREE from "three";
-import LightEnvironment from "../light/LightEnvironment";
-import PlayerController from "../player/PlayerController";
-import Process from "../runtime/Process";
-import PlayerCamera from "../player/PlayerCamera";
-import BoundingBox from "../props/BoundingBox";
-import PropInteractable from "../props/PropInteractable";
 
 export default class Index extends THREE.Scene {
   constructor() {
     super();
 
     this.background = new THREE.Color(0x323232);
-
-    BoundingBox.setMode('wireframe');
-    Process.setCamera(new PlayerCamera);
-
-    const floor = new BoundingBox(20, 0.1, 20);
-    floor.add(new THREE.Mesh(new THREE.BoxGeometry(20, 0.1, 20), new THREE.MeshBasicMaterial({ color: 0x4f4f4f })));
-    floor.isFloor = true;
-
-    this.add(floor);
-    this.add(new PlayerController);
-    this.add(new LightEnvironment(0xfafffb, 1.5));
   }
 }
